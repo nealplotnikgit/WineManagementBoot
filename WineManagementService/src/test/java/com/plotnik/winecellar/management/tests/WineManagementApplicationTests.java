1ff98c538f0e25261343b3e95b0938d70a1f5a02
nealplotnikgit <nealplotnik@gmail.com>
2019-03-26 22:04:05 -0500

    test

--- WineManagementService/src/test/java/com/plotnik/winecellar/management/tests/WineManagementApplicationTests.java
+++ WineManagementService/src/test/java/com/plotnik/winecellar/management/tests/WineManagementApplicationTests.java
@@ -1,16 +1,41 @@
 package com.plotnik.winecellar.management.tests;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.plotnik.winecellar.management.model.Wine;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WineManagementApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	private static Wine wine;
 	@Test
 	public void contextLoads() {
+		assertNotNull("the context loaded", this.applicationContext);
 	}
-
+	
+	@Test
+	public void createWine() {
+		assertNotNull("got wine", wine);
+		assertNotNull("wine has a name",wine.getName());
+		assertTrue("wine has a name as set",wine.getName().equals("RED"));
+	}
+	
+	@BeforeClass
+	public static void before() {
+		wine = new Wine();
+		wine.setName("RED");
+	}
 }
