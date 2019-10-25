71297e4c7b47e9fcafe40e1b18cdfe6363d374d1
nealplotnikgit <nealplotnik@gmail.com>
2019-10-24 21:53:24 -0500

    got auto-add of data to work. not pushed to pcf

---
+++ WineManagementService/src/main/resources/data-postgre.sql
@@ -0,0 +1,5 @@
+INSERT INTO wine (id, upc, category,cost, name, price, vintage_year, vintner)
+VALUES
+   (4, 082491091026, 'WHITE', 10.00, 'Chenin Blanc 750ML', 15.00, '2009', 'Dry Creek Vineyard'),
+   (5, 082491082024, 'WHITE', 12.00, 'Fume Blanc 750ML', 17.00, '2018', 'Dry Creek Vineyard'),
+   (6, 082491082017, 'WHITE', 6.00, 'Fume Blanc 375ML', 10.00, '2018', 'Dry Creek Vineyard');
